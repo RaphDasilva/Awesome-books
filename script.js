1f7893251df8473c48179c9bea64ca0538d89ee8
Mohammad Suliman joya <sjoya66@gmail.com>
2023-01-11 22:38:14 +0430

    Add  single page funtion

--- script.js
+++ script.js
@@ -61,7 +61,21 @@ addBtn.addEventListener('click', (e) => {
 });
 
 // single page function
-
+function displayList() {
+  document.getElementById('Book-list').classList.add('block');
+  document.getElementById('Add-new').classList.remove('block');
+  document.getElementById('contact').classList.remove('block');
+}
+function displayNew() {
+  document.getElementById('Add-new').classList.add('block');
+  document.getElementById('Book-list').classList.remove('block');
+  document.getElementById('contact').classList.remove('block');
+}
+function displayContact() {
+  document.getElementById('contact').classList.add('block');
+  document.getElementById('Book-list').classList.remove('block');
+  document.getElementById('Add-new').classList.remove('block');
+}
 
 // generate date in a particular format
 
