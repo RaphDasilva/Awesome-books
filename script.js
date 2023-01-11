82e26de8a7fdbebb0f7cfeaf10537179a881f093
Raphael <okolonnaemeka2017@gmail.com>
2023-01-11 10:58:11 -0500

    Refactor code to make remove button work.

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable max-classes-per-file */
-const bookList = document.querySelector('.booksList');
+const bookContainer = document.querySelector('.booksList');
 const form = document.querySelector('.form');
-const title = document.querySelector('.title');
-const author = document.querySelector('.author');
+const bookTitle = document.querySelector('.title');
+const bookAuthor = document.querySelector('.author');
 const addBtn = document.querySelector('#addBtn');
 
-const books = [];
+let bookList = [];
 
-class EachBook {
+class SingleBook {
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
 }
 
-class AddBook {
-  static putBook() {
-    const book = new EachBook(title.value, author.value);
-    books.push(book);
-  }
-
-  static bookUpload(arr) {
-    bookList.innerHTML = '';
-    for (let i = 0; i < arr.length; i += 1) {
-      bookList.innerHTML += `
+class DisplayBook {
+  static bookUpload() {
+    bookContainer.innerHTML = '';
+    for (let i = 0; i < bookList.length; i += 1) {
+      bookContainer.innerHTML += `
       <div class="book-wrapper">
-        <div class="book-info">
-        <p>"${arr[i].title}"</p>
-        <p>by</p>
-        <p>${arr[i].author}</p>
-        </div>
-        <button id='delete' type='button' onclick = 'AddBook.deleted(${i})'>Remove</button>
-        </div>
-       `;
-      title.value = '';
-      author.value = '';
+      <div class="book-info">
+      <p>"${bookList[i].title}"</p>
+      <p>by</p>
+      <p>${bookList[i].author}</p>
+      </div>
+      <button id='delete' type='button' onclick = 'DisplayBook.deleted(${i})'>Remove</button>
+      </div>
+     `;
+      bookTitle.value = '';
+      bookAuthor.value = '';
     }
   }
 
   static deleted(index) {
-    books.splice(index, 1);
-    AddBook.bookUpload(books);
-    localStorage.setItem('bookList', JSON.stringify(books));
+    bookList.splice(index, 1);
+    DisplayBook.bookUpload();
+    localStorage.setItem('bookList', JSON.stringify(bookList));
+  }
+
+  static addBook() {
+    const books = new SingleBook(bookTitle.value, bookAuthor.value);
+    bookList.push(books);
   }
 }
 
 window.onload = () => {
   if (localStorage.getItem('bookList')) {
-    const bookArr = JSON.parse(localStorage.getItem('bookList'));
-    AddBook.bookUpload(bookArr);
+    bookList = JSON.parse(localStorage.getItem('bookList'));
   }
+  DisplayBook.bookUpload();
 };
 
-addBtn.addEventListener('click', () => {
-  if (title.value === '' || author.value === '') return;
-  AddBook.putBook();
-  AddBook.bookUpload(books);
-  localStorage.setItem('bookList', JSON.stringify(books));
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  DisplayBook.addBook();
+  DisplayBook.bookUpload();
+  localStorage.setItem('bookList', JSON.stringify(bookList));
 });
