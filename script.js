8a2374d7de0ce6bba5a1e770b4e8438c38790d93
Mohammad Suliman joya <sjoya66@gmail.com>
2023-01-11 19:12:20 +0430

    Update the js with adding classes

--- script.js
+++ script.js
@@ -1,58 +1,17 @@
-const bookList = document.querySelector('.booksList');
-const form = document.querySelector('.form');
-const title = document.querySelector('.title');
-const author = document.querySelector('.author');
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-classes-per-file */
+const bookList = document.querySelector(".booksList");
+const form = document.querySelector(".form");
+const title = document.querySelector(".title");
+const author = document.querySelector(".author");
+const addBtn = document.querySelector("#addBtn");
 
-let books = [];
+const books = [];
 
-const addBook = (book) => {
-  bookList.innerHTML += `
-    <tr id='${book.id}'>
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td><button id='delete' type='button' onclick = 'removeBook(${book.id})'>Remove</button></td>
-    </tr>
-`;
-};
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  if (title.value && author.value !== '') {
-    const book = {
-      id: Math.random(),
-      title: title.value,
-      author: author.value,
-    };
-
-    if (localStorage.getItem('books') === null) {
-      books = [];
-    } else {
-      books = JSON.parse(localStorage.getItem('books'));
-    }
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-    addBook(book);
-    form.reset();
+class EachBook {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
-});
-
-// remove book
-
-function removeBook(bookId) {
-  const deleteBook = JSON.parse(localStorage.getItem('books'));
-
-  const newArray = deleteBook.filter((book) => book.id !== bookId);
-  localStorage.setItem('books', JSON.stringify(newArray));
-
-  const booksId = document.getElementById(bookId);
-  booksId.remove();
 }
 
-const storedBooks = JSON.parse(localStorage.getItem('books')) || [];
-
-if (localStorage.getItem('books')) {
-  storedBooks.map((book) => addBook(book));
-}
-
-removeBook();
